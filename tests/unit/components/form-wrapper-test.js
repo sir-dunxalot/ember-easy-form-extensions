f86ab95a7ba6d8dfd52291e2459fbc278d98d47b
Duncan Graham Walker <walkerdu@bc.edu>
2015-07-09 14:49:38 +0100

    Unit test for form wrapper component"

--- tests/unit/components/form-wrapper-test.js
+++ tests/unit/components/form-wrapper-test.js
@@ -1,5 +1,8 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
-import { setupComponent } from '../../helpers/unit/component';
+import { renderingTests } from '../../helpers/unit/component';
+
+const { typeOf } = Ember;
 
 let component;
 
@@ -11,13 +14,36 @@ moduleForComponent('form-wrapper', 'Unit | Component | form wrapper', {
   },
 });
 
-test('it renders', function(assert) {
+test('Rendering', function(assert) {
 
-  assert.equal(component._state, 'preRender',
-    'The component instance should be created');
+  assert.expect(2);
+
+  renderingTests(assert, this, component);
+});
+
+test('Properties', function(assert) {
+
+  assert.expect(5);
+
+  assert.notOk(component.get('novalidate'),
+    'The novalidate property should be false by default because Ember Validation provides client-side validations');
+
+  assert.ok(!!compoennt.get('className'),
+    'The component should have a default class name');
+
+  assert.ok(typeOf(component.get('formIsSubmitted')) === 'boolean',
+    'The component should have a boolean formIsSubmitted property bound for ease of use');
+
+  assert.ok(typeOf(component.get('formController')) === 'instance',
+    'The component should have a formController instance bound');
 
   this.render();
 
-  assert.equal(component._state, 'inDOM',
-    'The component should be inserted into the DOM after render');
+  assert.ok(component.$().hasClass(component.get('className')),
+    'The className property should be bound');
+
 });
+
+test('Methods', function() {
+    /* TODO - use inline template compiler to check autofocus functionality */
+  });
