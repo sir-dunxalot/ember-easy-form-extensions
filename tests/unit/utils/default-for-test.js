c56167353a148d339755caf2aa61900507e3413e
Duncan Graham Walker <walkerdu@bc.edu>
2015-07-12 00:39:45 -0400

    defaultFor unit test

--- tests/unit/utils/default-for-test.js
+++ tests/unit/utils/default-for-test.js
@@ -3,8 +3,35 @@ import { module, test } from 'qunit';
 
 module('Unit | Utility | default for');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  var result = defaultFor();
-  assert.ok(true);
+test('Correct values are returned', function(assert) {
+
+  assert.expect(11);
+
+  /* Test that fallbacks are used for the desired references */
+
+  assert.strictEqual(defaultFor(null, true), true,
+    'null should result in fallback');
+
+  assert.strictEqual(defaultFor(undefined, true), true,
+    'undefined should result in fallback');
+
+  /* Test what we expect to not need a fallback value */
+
+  [
+    'abc',
+    123,
+    {},
+    [],
+    function() {},
+    new Date,
+    false,
+    true,
+    0
+  ].forEach(function(expectedPositive) {
+
+    assert.strictEqual(defaultFor(expectedPositive, true), expectedPositive,
+      'The test should result in no fallback');
+
+  });
+
 });
